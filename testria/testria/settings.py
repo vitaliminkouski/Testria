03faafefbd24405668d573b7443e8326bf41f406
vitaliminkouski <vitaliminkouski@gmail.com>
2025-07-10 13:34:03 +0300

    implemented password recovery

--- testria/testria/settings.py
+++ testria/testria/settings.py
@@ -133,4 +133,14 @@ AUTHENTICATION_BACKENDS=[
     'users.authentication.EmailAuthBackend',
 ]
 
+# Email Settings for Password Reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'testriamail@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 from .private_settings import *
--- testria/users/urls.py
+++ testria/users/urls.py
@@ -1,10 +1,31 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 app_name = 'users'
 urlpatterns = [
     path('register/', views.RegisterUserView.as_view(), name='register'),
     path('login/', views.LoginUserView.as_view(), name='login'),
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
-    path('home/', views.index, name='home')
+    path('home/', views.index, name='home'),
+
+    path('password-reset/', PasswordResetView.as_view(
+        template_name='users/password_reset_form.html',
+        email_template_name='users/password_reset_email.html',
+        success_url=reverse_lazy('users:password_reset_done')
+    ), name='password_reset'),
+
+    path('password-reset/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html',
+         success_url=reverse_lazy('users:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('password-reset/complete',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete')
+
+
 ]
