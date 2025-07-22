e49f64edd426498e315cbb69a545568fc2f80544
vitaliminkouski <vitaliminkouski@gmail.com>
2025-07-22 14:40:54 +0300

    Started developing main logic. Created models

--- testria/testria/settings.py
+++ testria/testria/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'mainapp.apps.MainappConfig',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
