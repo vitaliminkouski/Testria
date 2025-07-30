f925a7fe5c8b76543fdfd2a4124228fed0c85cd7
vitaliminkouski <vitaliminkouski@gmail.com>
2025-07-30 16:09:48 +0300

    implemented folders deletion

--- testria/mainapp/urls.py
+++ testria/mainapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('create-folder/', views.CreateFolderView.as_view(), name='create_folder'),
+    path('foldera/create/', views.CreateFolderView.as_view(), name='create_folder'),
     path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder_detail'),
+    path('folders/<int:parent_folder_pk>/create/', views.CreateSubfolderView.as_view(), name='create_subfolder'),
+    path('folders/<int:pk>/delete/', views.DeleteFolderView.as_view(), name='delete_folder'),
 ]
