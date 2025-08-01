4ad3a3ed03e2d9f404ceef84dfa4323f73962641
vitaliminkouski <vitaliminkouski@gmail.com>
2025-08-01 19:40:38 +0300

    added sets

--- testria/mainapp/urls.py
+++ testria/mainapp/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('foldera/create/', views.CreateFolderView.as_view(), name='create_folder'),
+    path('folders/create/', views.CreateFolderView.as_view(), name='create_folder'),
     path('folders/<int:pk>/', views.FolderDetailView.as_view(), name='folder_detail'),
+    path('folders/<int:pk>/edit/', views.EditFolderView.as_view(), name='edit_folder'),
     path('folders/<int:pk>/delete/', views.DeleteFolderView.as_view(), name='delete_folder'),
     path('set/create/<int:folder_pk>/', views.CreateSetView.as_view(), name='create_set'),
+    path('set/<int:pk>/delete/', views.DeleteSetView.as_view(), name='delete_set'),
     path('set/', views.SetListView.as_view(), name='set_list')
 ]
